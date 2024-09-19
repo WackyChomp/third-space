7dfd596183b2fafd53956db48407e6ac6057686a
Wilson Chan <wilsonchan54@gmail.com>
2024-09-18 21:28:18 -0400

    Created constants index.ts / changed color styling for nav in index.css / added menu toggle and adjusted styling in navbar component

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import { useState } from 'react'
+import { navLinks } from '../constants'
 
 type Props = {}
+
+
+const NavItems = ({ onClick = () => {} }) => (
+  <ul className='nav-ul'>
+    {navLinks.map(({ id, name, href }) =>(
+      <li key={id} className='nav-li'>
+        <a href={href} className='nav-li-a' onClick={onClick}>
+          {name}
+        </a>
+      </li>
+    ))}
+  </ul>
+)
 
 const Navbar = (props: Props) => {
   const hamburgerIcon = `data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLW1lbnUiPjxsaW5lIHgxPSI0IiB4Mj0iMjAiIHkxPSIxMiIgeTI9IjEyIi8+PGxpbmUgeDE9IjQiIHgyPSIyMCIgeTE9IjYiIHkyPSI2Ii8+PGxpbmUgeDE9IjQiIHgyPSIyMCIgeTE9IjE4IiB5Mj0iMTgiLz48L3N2Zz4=`
@@ -9,6 +23,8 @@ const Navbar = (props: Props) => {
 
   const [count, setCount] = useState(-10);
 
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen( (prevIsOpen) => !prevIsOpen);
 
   return (
     <header className='fixed top-0 left-0 right-0 z-50 bg-red-600/40'>
@@ -19,21 +35,36 @@ const Navbar = (props: Props) => {
             Third Space
           </a>
 
-          <h1 className='text-2xl text-white underline'>Story of my life</h1>
+          <div className="bg-yellow-400 flex gap-5 p-0.5">
+            <h1 className='bg-blue-800 text-2xl text-white underline'>Story of my life</h1>
+
+            <button className='bg-pink-800 p-1'
+              onClick={() => setCount((count) => count + 2*2)}
+            >
+              count is currently: {count}
+            </button>
+          </div>
 
           <button 
-            className='bg-white-600'
-            onClick={() => setCount((count) => count + 2*2)}
+            onClick={toggleMenu}
+            className='text-green-600 hover:text-yellow-500 focus:outline-none sm:hidden flex'
+            aria-label='Toggle Menu'
           >
-            count is currently: {count}
+            <img src={isOpen ? closeIcon : hamburgerIcon} alt="toggle-burger" className='w-6 h-6' />
           </button>
 
-          <button >
-            <img src={hamburgerIcon} alt="toggle-burger" className='w-6 h-6' />
-          </button>
-
+          <nav className='sm:flex hidden'>
+            <NavItems />
+          </nav>
         </div>
       </div>
+
+      <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+        <nav className='p-5'>
+          <NavItems />
+        </nav>
+      </div>
+
     </header>
   )
 }
